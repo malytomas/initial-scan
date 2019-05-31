bab4f88a873c8168aa84745a10df41e9c243f244
Tomas Maly <malytomas@ucpu.cz>
2019-06-01 01:44:49 +0200

    actually start at player position

--- control.lua
+++ control.lua
@@ -1,38 +1,46 @@
-Scan_size = 128 --Scan size in chunks
+scan_queue_global = {}
+
+function generate_queue(position)
+  local res = {}
+  local xx = position.x / 32
+  local yy = position.y / 32
+  for r = 300, 7, -1 do
+    for x = r, -r, -1 do
+      table.insert(res, {(x + xx) * 32, -r * 32})
+      table.insert(res, {(x + xx) * 32,  r * 32})
+    end
+    for y = r, -r, -1 do
+      table.insert(res, {-r * 32, (y + yy) * 32})
+      table.insert(res, { r * 32, (y + yy) * 32})
+    end
+  end
+  return res
+end
 
 script.on_event(defines.events.on_player_created, function(event)
-  if event.player_index == 1 then
-    global.scan_starting_area = true
-  end
+  local player = game.players[event.player_index]
+  local scan = {}
+  scan.force = player.force
+  scan.surface = player.surface
+  scan.queue = generate_queue(player.position)
+  scan_queue_global[event.player_index] = scan
 end)
 
 script.on_event(defines.events.on_tick, function(event)
-  scan()
-  finished()
-end)
-
-function scan()
-  if not global.scan_starting_area then return end
-  local player = game.players[1]
-  local f = player.force
-  local s = player.surface
-  local z = Scan_size/2
-  for k = 6,z do
-    if game.tick ==((k^2)*24) - 10*60 then 
-      f.chart(s, {{-32*k, -32*k}, {32*k, 32*k}})
-      if k == z then
-        global.scan_starting_area = nil
-        Scan_size = nil
-        global.print_finished_message = game.tick + 10*60
+  if game.tick % 5 == 2 then
+    for index, scan in pairs(scan_queue_global) do
+      if #scan.queue == 0 then
+        local player = game.players[index]
+        if player ~= nil then
+          player.print("Initial map scan done")
+        end
+        table.remove(scan_queue_global, index)
+      else
+        local h = scan.queue[#scan.queue]
+        table.remove(scan.queue)
+        scan.force.chart(scan.surface, {h, h})
       end
-      return
     end
   end
-end
+end)
 
-function finished()
-  if not global.print_finished_message then return end
-  if game.tick ~= global.print_finished_message then return end
-  game.print("Initial map scan is finished")
-  global.print_finished_message = nil
-end
